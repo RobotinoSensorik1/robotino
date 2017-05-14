fc6e686a8b2088f442cda61f0ac13e2f5f842e87
Saskia <Saskia@Home-PC>
2017-05-14 13:39:35 +0200

    new commit Signed-off-by:Saskia <Saskia@Home-PC>

--- src/robotino/Robot.java
+++ src/robotino/Robot.java
@@ -5,52 +5,53 @@
  */
 package robotino;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.util.ArrayList;
 import rec.robotino.api2.Bumper;
 import rec.robotino.api2.Com;
 import rec.robotino.api2.OmniDrive;
 
-/**
- *
- * @author Saskia
- */
-public class Robot extends JFrame implements ActionListener {
-
-    JButton weiter = new JButton("weiter");
-    JButton stop = new JButton("stop");
+public class Robot {
 
     protected final Com _com;
     protected final OmniDrive _omniDrive;
     protected final Bumper _bumper;
     protected Command command = new Command();
+    protected final ArrayList<Sensor> sensors;
+    private ArrayList<Sensor> detectedSensors = new ArrayList<Sensor>();
+    private boolean drive = false;
 
     public Robot() {
-        weiter.addActionListener(this);
-        stop.addActionListener(this);
-        this.setLayout(new FlowLayout());
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().add(weiter, FlowLayout.LEFT);
-        this.getContentPane().add(stop, FlowLayout.LEFT);
-        this.setSize(300, 200);
-        this.setBounds(400, 400, 402, 402);
-        this.pack();
-        this.setVisible(true);
-
         _com = new MyCom();
         _omniDrive = new OmniDrive();
         _bumper = new Bumper();
+        sensors = new ArrayList<Sensor>();
 
-        _omniDrive.setComId(_com.id());
-        _bumper.setComId(_com.id());
+        // Sensoren anlegen als eigene Threads und den Thread starten
+        for (int i = 0; i < 9; i++) {
+            sensors.add(new Sensor(i, this._com));
+            sensors.get(i).start();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String hostname = "127.0.0.1";
+        //String hostname = "172.26.1.1";
+        if (args.length == 1) {
+            hostname = args[0].toString();
+        }
+
+        Robot robotino = new Robot();
+
+        try {
+            robotino.connect(hostname, true);
+            robotino.drive();
+            robotino.disconnect();
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
     }
 
     public boolean isConnected() {
@@ -67,104 +68,78 @@ public class Robot extends JFrame implements ActionListener {
         _com.disconnectFromServer();
     }
 
-    public void setVelocity(float vx, float vy, float omega) {
-        _omniDrive.setVelocity(vx, vy, omega);
-    }
-
-    
-
-    public void rotate(float[] inArray, float[] outArray, float deg) {
-        float rad = 2 * (float) Math.PI / 360.0f * deg;
-        outArray[0] = (float) Math.cos(rad) * inArray[0] - (float) Math.sin(rad) * inArray[1];
-        outArray[1] = (float) Math.sin(rad) * inArray[0] + (float) Math.cos(rad) * inArray[1];
-    }
-
     public void drive() throws InterruptedException {
         System.out.println("Driving...");
         float[] startVector = new float[]{0.0f, 0.1f};
         float[] dir = new float[2];
         float a = 0.0f;
-        int i= 0;
-        while (_com.isConnected() && false == _bumper.value() && i < 10) {
-            //rotate 360degrees in 5s
-//            rotate(startVector, dir, a);
-//            a = 360.0f * _com.msecsElapsed() / 5000;
-            
-                command.driveForward(dir, dir);
+        while (_com.isConnected() && !_bumper.value()) {
+
+            a = command.speedUpOfSpeedUp(0.00005f, 0.1f);
+            command.driveForward(dir, dir, a);
+            _omniDrive.setVelocity(dir[0], dir[1], 0);
+            while (isHindernis()) {
+
                 
-                _omniDrive.setVelocity(dir[0], dir[1], 0);
-                i++;
-                Thread.sleep(100);
-            
-        }
-    }
+                if(detectedSensors.size() == 1){
+                    //rotate in place
+                    //get angle to rotate
+                    float deg = command.getAngleToRotate(detectedSensors.get(0));
+                    float w = command.getAngularSpeed(deg);
+                    a = command.speedUpOfSpeedUp(0.0005f, 0.1f);
+                    command.rotateInPlace(dir, w);
+                    _omniDrive.setVelocity(a * 0.0f, a * 0.0f, w);
+                    Thread.sleep(100);
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("weiter")) {
-            try {
-                drive();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Robot.class.getName()).log(Level.SEVERE, null, ex);
+                    //geradeausfahren
+                    a = command.speedUpOfSpeedUp(0.00005f, 0.1f);
+                    command.driveForward(dir, dir, a);
+                    _omniDrive.setVelocity(dir[0], dir[1], 0);
+                    detectedSensors.clear();
+                    break;
+                }
+                else if (detectedSensors.size() == 2) {
+                    //check for free side
+                    float deg = command.getAngleToRotate(detectedSensors, 2);
+
+                } else if(detectedSensors.size() > 2){
+                    float deg = command.getAngleToRotate(detectedSensors, detectedSensors.size());
+                }
+
             }
-        } else if (e.getActionCommand().equals("stop")) {
-            System.exit(0);
+
+            Thread.sleep(100);
 
         }
     }
 
-    /**
-     * The class MyCom derives from rec.robotino.api2.Com and implements some of
-     * the virtual event handling methods. This is the standard approach for
-     * handling these Events.
-     */
-    class MyCom extends Com {
+    public boolean isHindernis() {
+        boolean hindernis = false;
+        for (Sensor s : sensors) {
+            if (s.getDistance() < 0.409) {
+                detectedSensors.add(s);
+                System.out.println(s.getNumberOfSensor()+" stopped by\t" + s.getDistance());
+                hindernis = true;
 
-        Timer _timer;
-
-        public MyCom() {
-            _timer = new Timer();
-            _timer.scheduleAtFixedRate(new OnTimeOut(), 0, 20);
-        }
-
-        class OnTimeOut extends TimerTask {
-
-            public void run() {
-                processEvents();
             }
         }
 
-        @Override
-        public void connectedEvent() {
-            System.out.println("Connected");
-        }
-
-        @Override
-        public void errorEvent(String errorStr) {
-            System.err.println("Error: " + errorStr);
-        }
-
-        @Override
-        public void connectionClosedEvent() {
-            System.out.println("Disconnected");
-        }
+        return hindernis;
     }
 
-    public static void main(String[] args) {
-        String hostname = "127.0.0.1";
-        if (args.length == 1) {
-            hostname = args[0].toString();
+    private Sensor getNearestSensor() {
+        if (detectedSensors != null) {
+            Sensor min = detectedSensors.get(0);
+            if (detectedSensors.size() > 1) {
+                for (Sensor s : detectedSensors) {
+                    if (s.getDistance() < min.getDistance()) {
+                        min = s;
+                    }
+                }
+            }
+            return min;
         }
-
-        Robot robotino = new Robot();
-
-        try {
-            robotino.connect(hostname, true);
-            robotino.drive();
-            robotino.disconnect();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
+        return null;
     }
 
 }
